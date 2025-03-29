7a23afd3b0800a7e18ff9c48759430573c57c683
Eric-Wubbo Lameijer <eric_wubbo@hotmail.com>
2025-03-29 11:31:39 +0100

    bad commit

--- src/app/book-list-item/book-list-item.component.ts
+++ src/app/book-list-item/book-list-item.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 import { Book } from '../shared/book';
 
 @Component({
-  selector: 'bm-book-list-item',
+  selector: 'book-list-item',
   imports: [],
   templateUrl: './book-list-item.component.html',
   styleUrl: './book-list-item.component.css',
